645cf17ed838d2aec93c08ddfbac001f34d348ae
riverfr0zen <riverfr0zen@elitemail.org>
2026-06-07 18:15:38 +0530

    radius modifiers

--- examples/circle_spokes/circle_spokes.gd
+++ examples/circle_spokes/circle_spokes.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
     var origin = rect.get_center()
     var spokes = []
     var inner_circle = get_circle_points(origin, 20, 50)
-    var outer_circle = get_circle_points(origin, 20, rect.size.y * 0.5)
+    #var outer_circle = get_circle_points(origin, 20, rect.size.y * 0.5)
+    var outer_circle = get_circle_points(origin, 20, rect.size.y * 0.5, 0.8, 1.0)
     for pos in range(inner_circle.size()):
         var spoke = Line2D.new()
         spoke.default_color = Color.RED
@@ -16,7 +17,7 @@ func _ready() -> void:
         add_child(spoke)
 
 
-func get_circle_points(origin: Vector2, count: int, r: float) -> Array[Vector2]:
+func get_circle_points(origin: Vector2, count: int, rad: float, rad_mod_low := 1.0, rad_mod_high := 1.0) -> Array[Vector2]:
     var result: Array[Vector2] = []
     
     for i in range(count):
@@ -24,7 +25,7 @@ func get_circle_points(origin: Vector2, count: int, r: float) -> Array[Vector2]:
         var angle = (i * TAU) / count
 
         # 1. Calculate the point offset from center
-        var offset = Vector2(cos(angle), sin(angle)) * r
+        var offset = Vector2(cos(angle), sin(angle)) * rad * randf_range(rad_mod_low, rad_mod_high)
 
         # 2. Add the origin to shift the point to the right place
         var point_position = origin + offset
