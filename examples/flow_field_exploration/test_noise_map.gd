1aceb81e635cf782b00368953adb4087a7c97721
riverfr0zen <riverfr0zen@elitemail.org>
2026-08-11 18:46:11 +0530

    examples of getting noise at once as an image instead of calculating per cell

--- examples/flow_field_exploration/test_noise_map.gd
+++ examples/flow_field_exploration/test_noise_map.gd
@@ -3,7 +3,7 @@ extends Node3D
 @export var animate := true
 @export var cell_alpha := 0.5
 @export var noise_freq := 0.1
-@export_enum("noise_3d", "noise_offset") var noise_method := "noise_3d" 
+@export_enum("noise_offset", "noise_3d", "get_image", "get_image3d") var noise_method := "get_image3d" 
 
 var noise : FastNoiseLite
 var time := 0.0
@@ -24,14 +24,42 @@ func _process(delta: float) -> void:
     if animate:
         time += delta * speed
 
-    for y in range($GridManager.grid_size.y):
-        for x in range($GridManager.grid_size.x):
-            var cell = $GridManager.get_cell_at(x, y)
-            #var r = noise.get_noise_2d(float(x), float(y))
-            var r: float
-            if noise_method == "noise_3d":
-                r = noise.get_noise_3d(float(x), float(y), time)
-            if noise_method == "noise_offset":
-                r = noise.get_noise_2d(float(x) + time, float(y) + time)
-            var cval = remap(r, -1.0, 1.0, 0.0, 1.0)
-            cell.color = Color(cval, cval, cval, cell_alpha)            
+    if noise_method == "noise_3d" || noise_method == "noise_offset":
+        for y in range($GridManager.grid_size.y):
+            for x in range($GridManager.grid_size.x):
+                var cell = $GridManager.get_cell_at(x, y)
+                #var r = noise.get_noise_2d(float(x), float(y))
+                var r: float
+                if noise_method == "noise_3d":
+                    r = noise.get_noise_3d(float(x), float(y), time)
+                if noise_method == "noise_offset":
+                    r = noise.get_noise_2d(float(x) + time, float(y) + time)
+                var cval = remap(r, -1.0, 1.0, 0.0, 1.0)
+                cell.color = Color(cval, cval, cval, cell_alpha)            
+
+
+    if noise_method == "get_image":
+        noise.offset = Vector3(time, time, 0.0)
+        var img: Image = noise.get_image($GridManager.grid_size.x, $GridManager.grid_size.y)
+        for y in range($GridManager.grid_size.y):
+            for x in range($GridManager.grid_size.x):
+                var cval = img.get_pixel(x, y).r
+                var cell = $GridManager.get_cell_at(x, y)
+                cell.color = Color(cval, cval, cval, cell_alpha)
+
+    if noise_method == "get_image3d":
+        noise.offset = Vector3(0, 0, time)
+        var slices: Array[Image] = noise.get_image_3d(
+            $GridManager.grid_size.x, 
+            $GridManager.grid_size.y, 
+            1, # depth (we only need 1 slice for time)
+            false, 
+            true # setting normalize to true can result in darker areas
+        )
+        var img: Image = slices[0]
+        #var img: Image = noise.get_image($GridManager.grid_size.x, $GridManager.grid_size.y)
+        for y in range($GridManager.grid_size.y):
+            for x in range($GridManager.grid_size.x):
+                var cval = img.get_pixel(x, y).r
+                var cell = $GridManager.get_cell_at(x, y)
+                cell.color = Color(cval, cval, cval, cell_alpha)
