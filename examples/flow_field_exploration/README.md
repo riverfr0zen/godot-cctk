09cc50e582ae4fd18a5c8ec033e615e26963086b
riverfr0zen <riverfr0zen@elitemail.org>
2026-08-11 18:11:19 +0530

    animating the noise

---
+++ examples/flow_field_exploration/README.md
@@ -0,0 +1,15 @@
+# Exploration notes
+
+These notes will indicate what each scene in this folder is, and how they fit into the exploration.
+
+
+# test_noise_map.tscn
+
+First exploration while looking at these videos:
+
+* https://www.youtube.com/watch?v=na7LuZsW2UM
+* https://www.youtube.com/watch?v=BjoM9oKOAKY
+
+This is scene is just exploring how to use Godot's noise functions and displays a noise map.
+
+Different ways to get and animate the noise are explored.
--- examples/flow_field_exploration/test_noise_map.gd
+++ examples/flow_field_exploration/test_noise_map.gd
@@ -1,10 +1,13 @@
 extends Node3D
 
+@export var animate := true
 @export var cell_alpha := 0.5
 @export var noise_freq := 0.1
+@export_enum("noise_3d", "noise_offset") var noise_method := "noise_3d" 
 
 var noise : FastNoiseLite
-
+var time := 0.0
+var speed := 10.0
 
 func _ready() -> void:
     noise = FastNoiseLite.new()
@@ -17,10 +20,18 @@ func _ready() -> void:
     # Center the grid
     $GridManager.position_center()
     
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+    if animate:
+        time += delta * speed
+
     for y in range($GridManager.grid_size.y):
         for x in range($GridManager.grid_size.x):
             var cell = $GridManager.get_cell_at(x, y)
-            var r = noise.get_noise_2d(float(x), float(y))
+            #var r = noise.get_noise_2d(float(x), float(y))
+            var r: float
+            if noise_method == "noise_3d":
+                r = noise.get_noise_3d(float(x), float(y), time)
+            if noise_method == "noise_offset":
+                r = noise.get_noise_2d(float(x) + time, float(y) + time)
             var cval = remap(r, -1.0, 1.0, 0.0, 1.0)
             cell.color = Color(cval, cval, cval, cell_alpha)            
