adfdfe01bf24e5f127670459fb1b6728a21da701
riverfr0zen <riverfr0zen@elitemail.org>
2026-08-13 11:11:32 +0530

    added FlowFieldHud and using it in the test

--- examples/flow_field_exploration/test_flow_field_hud.gd
+++ examples/flow_field_exploration/test_flow_field_hud.gd
@@ -5,24 +5,17 @@ const FREQ_INC := 0.01
 var flow_field : FlowField2D
 
 func _ready() -> void:
-    # For this example, I'm setting the flow field size to half the size of the grid
-    # so that we can test the scale parameter later during the `sample_vector` call.
-    flow_field = FlowField2D.new($GridManager.grid_size / 2)
+    flow_field = FlowField2D.new(Vector2(40, 30))
     flow_field.speed = 2
     flow_field.frequency = 0.05
     #flow_field.normalize = false
-    $GridManager.position_center()
-    $GridManager.hide_grid = true
-
+    $FlowFieldHud.position_center()
+    # Setting the flow_field on the hud will also update the hud's grid size
+    $FlowFieldHud.flow_field = flow_field
 
 func _process(delta: float) -> void:
     flow_field.update(delta)
-    
-    for y in range($GridManager.grid_size.y):
-        for x in range($GridManager.grid_size.x):
-            var angle_vec = flow_field.sample_vector(Vector2(x, y), Vector2(2, 2))
-            var cell = $GridManager.get_cell_at(x, y)
-            cell.rotation = angle_vec.angle()
+    $FlowFieldHud.update()
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("common.restart"):
